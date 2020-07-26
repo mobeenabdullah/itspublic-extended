95b14ffb5f599fb0143da410574202debd262ffa
Mobeen Abdullah <mobeenabdullah@gmail.com>
2020-07-27 00:24:42 +0500

    changes excerpt length

--- public/partials/itspublic-extended-extra.php
+++ public/partials/itspublic-extended-extra.php
@@ -2,6 +2,6 @@
 
 // Trimming Excerpt Length for Teams and Projects
 function itspublic_custom_excerpt_length( $length ) {
-	return 25;
+	return 20;
 }
 add_filter( 'excerpt_length', 'itspublic_custom_excerpt_length', 999 );
