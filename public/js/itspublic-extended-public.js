6d2edb0aa063cdc4435074e07cf2593798ed3c8a
Mobeen Abdullah <mobeenabdullah@gmail.com>
2020-09-04 23:48:49 +0500

    closing popup on pressing ESC key

--- public/js/itspublic-extended-public.js
+++ public/js/itspublic-extended-public.js
@@ -386,6 +386,13 @@
 		}
 		placeholder();
 
+		// Hiding Popup on ESC Key
+		$( document ).on( 'keydown', function ( e ) {
+			if ( e.keyCode === 27 ) { // ESC
+				$( '.modal' ).hide();
+			}
+		});
+
 	});
 
 
@@ -409,15 +416,15 @@ window.onclick = function (e) {
 	}
 };
 
-document.onkeydown = function(evt) {
-	evt = evt || window.event;
-	var isEscape = false;
-	if ("key" in evt) {
-		isEscape = (evt.key === "Escape" || evt.key === "Esc");
-	} else {
-		isEscape = (evt.keyCode === 27);
-	}
-	if (isEscape) {
-		document.querySelector('.modal').style.display = "none";
-	}
-};
+// document.onkeydown = function(evt) {
+// 	evt = evt || window.event;
+// 	var isEscape = false;
+// 	if ("key" in evt) {
+// 		isEscape = (evt.key === "Escape" || evt.key === "Esc");
+// 	} else {
+// 		isEscape = (evt.keyCode === 27);
+// 	}
+// 	if (isEscape) {
+// 		document.querySelector('.modal').style.display = "none";
+// 	}
+// };
