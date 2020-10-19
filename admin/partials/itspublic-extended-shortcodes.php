5c7aad151e1a02e98b55ed9f83920e7c8e37cac6
Mobeen Abdullah <mobeenabdullah@gmail.com>
2020-10-19 16:55:37 +0500

    static form action url change to dynamic

--- admin/partials/itspublic-extended-shortcodes.php
+++ admin/partials/itspublic-extended-shortcodes.php
@@ -207,7 +207,7 @@ function itspublic_hero_search_form() {
 	ob_start(); ?>
 
     <div class="searchbox__cover">
-        <form action="http://itspublic.local/materialen/?ms=a" class="searchbox__cover-form" method="get">
+        <form action="<?php echo home_url(); ?>/materialen/?ms=a" class="searchbox__cover-form" method="get">
             <div class="searchbox__cover-form--input">
                 <input type="text" value="" class="search-input" id="search_field" placeholder="Doorzoek hier onze publicaties. Zoek bijvoorbeeld op 'onderwijs'" />
                 <button type="submit" class="search-btn">
